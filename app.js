99113226f223ea393bfa8a036affd18f998da544
cristineshipman <cristine.shipman@gmail.com>
2017-08-08 11:14:26 -0500

    try again with port

--- app.js
+++ app.js
@@ -128,6 +128,6 @@ app.delete("/blogs/:id", function(req, res){
   });
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("magic is happening")
 });
